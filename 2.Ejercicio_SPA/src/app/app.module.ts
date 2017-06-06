d2b66bfd5633d326c7ca9a396606cfa3b7506b3d
Jesus Laucho <jlaucho@gmail.com>
2017-06-06 14:58:03 -0400

    Creando rutas

--- 2.Ejercicio_SPA/src/app/app.module.ts
+++ 2.Ejercicio_SPA/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { app_routing } from './app.routes';
+//Servicios
+
+//Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './component/shared/navbar/navbar.component';
 import { HomeComponent } from './component/home/home.component';
@@ -16,7 +21,8 @@ import { HeroesComponent } from './component/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
