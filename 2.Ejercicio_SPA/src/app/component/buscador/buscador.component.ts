0f7773ff4301c53a95bcbbd364d4da278b536d09
Jesus Laucho <jlaucho@gmail.com>
2017-06-09 08:42:43 -0400

    Para unir

--- 2.Ejercicio_SPA/src/app/component/buscador/buscador.component.ts
+++ 2.Ejercicio_SPA/src/app/component/buscador/buscador.component.ts
@@ -8,7 +8,7 @@ import { Router }             from '@angular/router';
   templateUrl: './buscador.component.html'
 })
 export class BuscadorComponent implements OnInit {
-
+  palabra:string;
   heroes:any[] = [];
   constructor( private _activRoute:ActivatedRoute,
               private _heroeService:HeroesService,
@@ -16,6 +16,7 @@ export class BuscadorComponent implements OnInit {
 
   ngOnInit() {
     this._activRoute.params.subscribe( params=>{
+      this.palabra = params.texto;
       this.heroes = this._heroeService.buscarHeroes( params.texto );
       console.log(this.heroes);
     })
